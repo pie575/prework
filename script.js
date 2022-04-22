d8ea24fd0ab811a770ec75f3010d9e06fd0662ec
Glitch (glitch-hello-website) <none>
2022-04-22 22:52:50 +0000

    🌍👍 Checkpoint ./script.js:44137059/39

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ Author-Jackie Chen
 // Global Variables
 const nextClueWaitTime = 1000; //how long to wait before starting playback of the clue sequence
 const cluePauseTime = 333; //how long to pause in between clues
-const buttonLen = 6;
+const numButtons = 4;
 
-var pattern = []
+var pattern = [0,0,0,0,0,0,0,0]
+var score = 0
