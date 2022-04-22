201d160d071fe4012090906fab1e1c5200800a3a
Glitch (glitch-hello-website) <none>
2022-04-22 22:41:48 +0000

    🐀💐 Checkpoint ./script.js:44137059/223

--- script.js
+++ script.js
@@ -2,3 +2,9 @@
 Author-Jackie Chen
 */
 
+// Global Variables
+const nextClueWaitTime = 1000; //how long to wait before starting playback of the clue sequence
+const cluePauseTime = 333; //how long to pause in between clues
+const buttonLen = 6;
+
+var pattern = []
