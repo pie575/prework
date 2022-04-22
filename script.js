224710054f7c50c8cc298d10ec3ae2cbc1cf59cd
Glitch (glitch-hello-website) <none>
2022-04-22 23:40:48 +0000

    ✊🎅 Checkpoint ./script.js:44137059/1553

--- script.js
+++ script.js
@@ -6,12 +6,21 @@ Author-Jackie Chen
 const clueWaitTime = 333; //how long to pause in between clues
 const clueHoldTime = 1000;
 const numButtons = 4;
+const volume = 0.5;
+const frequencies = [415,310,252,209]; // traditional simon says sounds
 
 var sequence = [0,0,0,0,0,0,0,0]; // initalized pattern for testing purposes
 var score = 0;
 var playing = false;
 var sound = false;
-var volume = 0.5;
+
+var context = new AudioContext();
+var o = context.createOscillator();
+var g = context.createGain();
+g.connect(context.destination);
+g.gain.setValueAtTime(0, context.currentTime);
+o.connect(g);
+o.start(0);
 
 
 // Functions for modularity
@@ -26,15 +35,26 @@ function startGame() {
   playing = true;
   score = 0;
 
-  document.getElementById("startBtn").classList.add("hidden");
-  document.getElementById("stopBtn").classList.remove("hidden");
+  document.getElementById("startButton").classList.add("hidden");
+  document.getElementById("stopButton").classList.remove("hidden");
   
   generateSequence();
   playGame();
 }
+
 function stopGame() {
-  gamePlaying = false;
+  playing = false;
   document.getElementById("startBtn").classList.remove("hidden");
   document.getElementById("stopBtn").classList.add("hidden");
-  reset=true;
 }
+
+function playSound(buttonNumber, length) {
+  o.frequency.value = frequencies[buttonNumber];
+  g.gain.setTargetAtTime(volume, context.currentTime + 0.05, 0.025);
+  sound = true;
+  setTimeout(function() {
+    stopTone();
+  }, length); 
+}
+
+// Init Sound Synthesizer
