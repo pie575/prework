50eb9deacd162973dbcadda5bd3ba6001d3469fe
Glitch (glitch-hello-website) <none>
2022-04-22 23:28:48 +0000

    🐋👣 Checkpoint ./script.js:44137059/1908

--- script.js
+++ script.js
@@ -3,12 +3,38 @@ Author-Jackie Chen
 */
 
 // Global Variables
-const nextClueWaitTime = 1000; //how long to wait before starting playback of the clue sequence
-const cluePauseTime = 333; //how long to pause in between clues
+const clueWaitTime = 333; //how long to pause in between clues
+const clueHoldTime = 1000;
 const numButtons = 4;
 
-var pattern = [0,0,0,0,0,0,0,0]; // initalized pattern for testing purposes
+var sequence = [0,0,0,0,0,0,0,0]; // initalized pattern for testing purposes
 var score = 0;
 var playing = false;
 var sound = false;
 var volume = 0.5;
+
+
+// Functions for modularity
+function generateSequence() {
+  // sequence of buttons the game will play
+  for (let i = 0; i < sequence.length; i++) {
+    sequence[i] = Math.ceil(Math.random() * numButtons);
+  }
+}
+
+function startGame() {
+  playing = true;
+  score = 0;
+
+  document.getElementById("startBtn").classList.add("hidden");
+  document.getElementById("stopBtn").classList.remove("hidden");
+  
+  generateSequence();
+  playGame();
+}
+function stopGame() {
+  gamePlaying = false;
+  document.getElementById("startBtn").classList.remove("hidden");
+  document.getElementById("stopBtn").classList.add("hidden");
+  reset=true;
+}
